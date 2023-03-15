b49f8cbdf66a73195e33f1a595c1ec61e1bfbabe
F-OBrien <francis@polymesh.network>
2023-03-15 08:00:20 +0000

    update polymesh types

---
+++ packages/apps-config/src/api/spec/polymesh-testnet.ts
@@ -0,0 +1,8 @@
+// Copyright 2017-2023 @polkadot/apps-config authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { typesBundle } from '@polymeshassociation/polymesh-types';
+
+const definitions = typesBundle.spec.polymesh_testnet;
+
+export default definitions;
